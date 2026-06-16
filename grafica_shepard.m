63f31d4c64d45cd38d48582e901694a959166bc1
franlopla <franlopla10@gmail.com>
2026-06-16 21:31:45 +0200

    Add files via upload

---
+++ grafica_shepard.m
@@ -0,0 +1,334 @@
+%%
+rng(1);
+f=@(x,y)exp(-2.5^2.*(x.^2+y.^2)./2);
+
+% Generamos valores aleatorios en el intervalo [-1,1]
+X_datos=-1+2.*rand([10,1]);
+Y_datos=-1+2.*rand([10,1]);
+Z_datos=f(X_datos,Y_datos);
+alpha=2;
+
+% Generamos una malla 50x50
+x_interp=linspace(-1,1,50);
+y_interp=linspace(-1,1,50);
+[X_malla,Y_malla]=meshgrid(x_interp,y_interp);
+
+[n,m]=size(X_malla);
+Z_interp=zeros(size(X_malla));
+
+% Evaluamos la funcion interpolante en los nodos de la malla
+for i=1:n
+    for j=1:m
+        px=X_malla(i,j);
+        py=Y_malla(i,j);
+        Z_interp(i,j)=shepard(X_datos,Y_datos,Z_datos,px,py,alpha);
+    end
+end
+
+% Generamos la grafica marcando los nodos que queremos interpolar en rojo
+% para comprobar que la superficie interpola a los nodos
+figure('Name','Interpolación Shepard');
+hold on;
+grid on;
+surf(X_malla,Y_malla,Z_interp,'EdgeColor','none','FaceAlpha',0.8);
+scatter3(X_datos,Y_datos,Z_datos,'MarkerEdgeColor','k','MarkerFaceColor','red');
+colormap(parula); 
+view(45,30);
+
+%%
+rng(1);
+f=@(x,y)exp(-2.5^2.*(x.^2+y.^2)./2);
+X_datos=-1+2.*rand([50,1]);
+Y_datos=-1+2.*rand([50,1]);
+
+Z_datos=f(X_datos,Y_datos);
+alpha=2;
+
+x_interp=linspace(-1,1,50);
+y_interp=linspace(-1,1,50);
+
+[X_malla,Y_malla]=meshgrid(x_interp,y_interp);
+[n,m]=size(X_malla);
+
+Z_interp=zeros(size(X_malla));
+
+for i=1:n
+    for j=1:m
+        px=X_malla(i,j);
+        py=Y_malla(i,j);
+        Z_interp(i,j)=shepard(X_datos,Y_datos,Z_datos,px,py,alpha);
+    end
+end
+
+figure('Name','Interpolación Shepard');
+hold on;
+grid on;
+surf(X_malla,Y_malla,Z_interp,'EdgeColor','none','FaceAlpha',0.8);
+scatter3(X_datos,Y_datos,Z_datos,'MarkerEdgeColor','k','MarkerFaceColor','red');
+colormap(parula); 
+view(45,30);
+
+%%
+rng(1);
+f=@(x,y)exp(-2.5^2.*(x.^2+y.^2)./2);
+X_datos=-1+2.*rand([150,1]);
+Y_datos=-1+2.*rand([150,1]);
+
+Z_datos=f(X_datos,Y_datos);
+alpha=2;
+
+x_interp=linspace(-1,1,50);
+y_interp=linspace(-1,1,50);
+
+[X_malla,Y_malla]=meshgrid(x_interp,y_interp);
+[n,m]=size(X_malla);
+
+Z_interp=zeros(size(X_malla));
+
+for i=1:n
+    for j=1:m
+        px=X_malla(i,j);
+        py=Y_malla(i,j);
+        Z_interp(i,j)=shepard(X_datos,Y_datos,Z_datos,px,py,alpha);
+    end
+end
+
+figure('Name','Interpolación Shepard');
+hold on;
+grid on;
+surf(X_malla,Y_malla,Z_interp,'EdgeColor','none','FaceAlpha',0.8);
+scatter3(X_datos,Y_datos,Z_datos,'MarkerEdgeColor','k','MarkerFaceColor','red');
+colormap(parula);
+view(45,30);
+
+%%
+rng(1);
+f=@(x,y)exp(-2.5^2.*(x.^2+y.^2)./2);
+X_datos=-1+2.*rand([500,1]);
+Y_datos=-1+2.*rand([500,1]);
+
+
+Z_datos=f(X_datos,Y_datos);
+alpha=2;
+
+x_interp=linspace(-1,1,50);
+y_interp=linspace(-1,1,50);
+
+[X_malla,Y_malla]=meshgrid(x_interp,y_interp);
+[n,m]=size(X_malla);
+
+Z_interp=zeros(size(X_malla));
+
+for i=1:n
+    for j=1:m
+        px=X_malla(i,j);
+        py=Y_malla(i,j);
+        Z_interp(i,j)=shepard(X_datos,Y_datos,Z_datos,px,py,alpha);
+    end
+end
+
+figure('Name','Interpolación Shepard', 'Color', 'w');
+hold on;
+grid on;
+surf(X_malla,Y_malla,Z_interp,'EdgeColor','none','FaceAlpha',0.8);
+scatter3(X_datos,Y_datos,Z_datos,'MarkerEdgeColor','k','MarkerFaceColor','red');
+colormap(parula); 
+view(45,30);
+
+%% Graficamos ahora la funcion exacta
+rng(1);
+f=@(x,y)exp(-2.5^2.*(x.^2+y.^2)./2);
+X_datos=-1+2.*rand([50,1]);
+Y_datos=-1+2.*rand([50,1]);
+
+
+Z_datos=f(X_datos,Y_datos);
+alpha=2;
+
+x_interp=linspace(-1,1,50);
+y_interp=linspace(-1,1,50);
+
+[X_malla,Y_malla]=meshgrid(x_interp,y_interp);
+[n,m]=size(X_malla);
+
+Z_interp=zeros(size(X_malla));
+
+for i=1:n
+    for j=1:m
+        px=X_malla(i,j);
+        py=Y_malla(i,j);
+        Z_interp(i,j)=shepard(X_datos,Y_datos,Z_datos,px,py,alpha);
+    end
+end
+
+
+figure('Name','GRAFICA FUNCION');
+hold on;
+grid on;
+surf(X_malla,Y_malla,f(X_malla,Y_malla),'EdgeColor','none','FaceAlpha',0.8);
+colormap(parula); 
+view(45,30);
+
+%%
+rng(1);
+f=@(x,y)exp(-2.5^2.*(x.^2+y.^2)./2);
+X_datos=-1+2.*rand([150,1]);
+Y_datos=-1+2.*rand([150,1]);
+
+Z_datos=f(X_datos,Y_datos);
+alpha=1;
+
+x_interp=linspace(-1,1,50);
+y_interp=linspace(-1,1,50);
+
+[X_malla,Y_malla]=meshgrid(x_interp,y_interp);
+[n,m]=size(X_malla);
+
+Z_interp=zeros(size(X_malla));
+
+for i=1:n
+    for j=1:m
+        px=X_malla(i,j);
+        py=Y_malla(i,j);
+        Z_interp(i,j)=shepard(X_datos,Y_datos,Z_datos,px,py,alpha);
+    end
+end
+
+figure('Name','Interpolación Shepard');
+hold on;
+grid on;
+surf(X_malla, Y_malla, Z_interp,'EdgeColor','none','FaceAlpha',0.8);
+scatter3(X_datos,Y_datos,Z_datos,'MarkerEdgeColor','k','MarkerFaceColor','red');
+colormap(parula);
+view(45,30);
+%%
+rng(1);
+f=@(x,y)exp(-2.5^2.*(x.^2+y.^2)./2);
+X_datos=-1+2.*rand([150,1]);
+Y_datos=-1+2.*rand([150,1]);
+
+Z_datos=f(X_datos,Y_datos);
+alpha=2;
+
+x_interp=linspace(-1,1,50);
+y_interp=linspace(-1,1,50);
+
+[X_malla,Y_malla]=meshgrid(x_interp,y_interp);
+[n,m]=size(X_malla);
+
+Z_interp=zeros(size(X_malla));
+
+for i=1:n
+    for j=1:m
+        px=X_malla(i,j);
+        py=Y_malla(i,j);
+        Z_interp(i,j)=shepard(X_datos,Y_datos,Z_datos,px,py,alpha);
+    end
+end
+
+figure('Name','Interpolación Shepard');
+hold on;
+grid on;
+surf(X_malla, Y_malla, Z_interp,'EdgeColor','none','FaceAlpha',0.8);
+scatter3(X_datos,Y_datos,Z_datos,'MarkerEdgeColor','k','MarkerFaceColor','red');
+colormap(parula); 
+view(45,30);
+%%
+rng(1);
+f=@(x,y)exp(-2.5^2.*(x.^2+y.^2)./2);
+X_datos=-1+2.*rand([150,1]);
+Y_datos=-1+2.*rand([150,1]);
+
+Z_datos=f(X_datos,Y_datos);
+alpha=5;
+
+x_interp=linspace(-1,1,50);
+y_interp=linspace(-1,1,50);
+
+[X_malla,Y_malla]=meshgrid(x_interp,y_interp);
+[n,m]=size(X_malla);
+
+Z_interp=zeros(size(X_malla));
+
+for i=1:n
+    for j=1:m
+        px=X_malla(i,j);
+        py=Y_malla(i,j);
+        Z_interp(i,j)=shepard(X_datos,Y_datos,Z_datos,px,py,alpha);
+    end
+end
+
+figure('Name','Interpolación Shepard');
+hold on;
+grid on;
+surf(X_malla,Y_malla,Z_interp,'EdgeColor','none','FaceAlpha',0.8);
+scatter3(X_datos,Y_datos,Z_datos,'MarkerEdgeColor','k','MarkerFaceColor','red');
+colormap(parula); 
+view(45, 30);
+%%
+rng(1);
+f=@(x,y)exp(-2.5^2.*(x.^2+y.^2)./2);
+X_datos=-1+2.*rand([150,1]);
+Y_datos=-1+2.*rand([150,1]);
+
+Z_datos=f(X_datos,Y_datos);
+alpha=10;
+
+x_interp=linspace(-1,1,50);
+y_interp=linspace(-1,1,50);
+
+[X_malla,Y_malla]=meshgrid(x_interp,y_interp);
+[n,m]=size(X_malla);
+
+Z_interp=zeros(size(X_malla));
+
+for i=1:n
+    for j=1:m
+        px=X_malla(i,j);
+        py=Y_malla(i,j);
+        Z_interp(i,j)=shepard(X_datos,Y_datos,Z_datos,px,py,alpha);
+    end
+end
+
+figure('Name','Interpolación Shepard');
+hold on;
+grid on;
+
+surf(X_malla,Y_malla,Z_interp,'EdgeColor','none','FaceAlpha',0.8);
+scatter3(X_datos,Y_datos,Z_datos,'MarkerEdgeColor','k','MarkerFaceColor','red');
+colormap(parula); 
+view(45, 30);
+%%
+rng(1);
+f=@(x,y)exp(-2.5^2.*(x.^2+y.^2)./2);
+X_datos=-1+2.*rand([150,1]);
+Y_datos=-1+2.*rand([150,1]);
+
+Z_datos=f(X_datos,Y_datos);
+alpha=20;
+
+x_interp=linspace(-1,1,50);
+y_interp=linspace(-1,1,50);
+
+[X_malla,Y_malla]=meshgrid(x_interp,y_interp);
+[n,m]=size(X_malla);
+
+Z_interp=zeros(size(X_malla));
+
+for i=1:n
+    for j=1:m
+        px=X_malla(i,j);
+        py=Y_malla(i,j);
+        Z_interp(i,j)=shepard(X_datos,Y_datos,Z_datos,px,py,alpha);
+    end
+end
+
+figure('Name','Interpolación Shepard');
+hold on;
+grid on;
+
+surf(X_malla,Y_malla,Z_interp,'EdgeColor','none','FaceAlpha',0.8);
+scatter3(X_datos,Y_datos,Z_datos,'MarkerEdgeColor','k','MarkerFaceColor','red');
+colormap(parula);
+view(45,30);
+
